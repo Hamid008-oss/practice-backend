d2a70b6f218c0fcc0e8c42a00e0cc16a38458362
Hamid008-oss <somro156@gmail.com>
2025-12-07 07:33:37 +0500

    utils code

--- src/index.js
+++ src/index.js
@@ -6,4 +6,12 @@ import connectDB from "./db/index.js";
 
 
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed", err);
+  })
